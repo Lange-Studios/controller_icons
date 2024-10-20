1cbbe43a119cda9f46e1cacc63c9012ca7f4346a
Travis Lange <travislange12@gmail.com>
2024-10-20 14:38:24 -0400

    renamed icon_texture_rec_packed to icon_texture_rect_packed

--- addons/controller_icons/objects/ControllerIconBindsContainer.gd
+++ addons/controller_icons/objects/ControllerIconBindsContainer.gd
@@ -7,7 +7,7 @@ class_name ControllerIconBindsContainer
 		path = _path
 		_on_input_type_changed(ControllerIcons._last_input_type, ControllerIcons._last_controller)
 
-@export var icon_texture_rec_packed: PackedScene
+@export var icon_texture_rect_packed: PackedScene
 @export var seperator_packed: PackedScene
 
 func _init():
@@ -22,14 +22,15 @@ func _on_input_type_changed(input_type: int, controller: int):
 		child.queue_free()
 	
 	for i in range(0, count):
-		if i > 0:
+		if i > 0 and seperator_packed != null:
 			var seperator = seperator_packed.instantiate()
 			add_child(seperator)
 		
-		var icon: TextureRect = icon_texture_rec_packed.instantiate()
-		icon.texture = icon.texture.duplicate()
-		var texture: ControllerIconTexture = icon.texture
-		texture.path = path
-		texture.index = i
-		add_child(icon)
+		if icon_texture_rect_packed != null:
+			var icon: TextureRect = icon_texture_rect_packed.instantiate()
+			icon.texture = icon.texture.duplicate()
+			var texture: ControllerIconTexture = icon.texture
+			texture.path = path
+			texture.index = i
+			add_child(icon)
 	
